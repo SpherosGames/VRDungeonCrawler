87660e6bbff2b7cc8771ef5cb42e3264fe45f0b0
SpherosGames <jbol1@st.noorderpoort.nl>
2024-09-24 11:19:31 +0200

    Improved grabbing

--- Assets/Main/Scripts/VR/HandPhysics.cs
+++ Assets/Main/Scripts/VR/HandPhysics.cs
@@ -1,16 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using System.Linq;
+using static UnityEngine.Rendering.DebugUI;
 
 public class HandPhysics : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private XRDirectInteractor interactor;
+    [SerializeField] private PhysicMaterial handMaterial;
+    [SerializeField] private float colliderEnableDelay = 1;
+
+    private Collider[] colliders;
+
+    private void Awake()
+    {
+        colliders = GetComponentsInChildren<Collider>();
+        colliders.ToList().ForEach(x => x.material = handMaterial);
+    }
 
     private void OnEnable()
     {
         //Set rigidbody
         if (rb == null) rb = GetComponent<Rigidbody>();
+
+        interactor.selectEntered.RemoveAllListeners();
+        interactor.selectEntered.AddListener((SelectEnterEventArgs args) => SetColliders(false, 0));
+
+        interactor.selectExited.RemoveAllListeners();
+        interactor.selectExited.AddListener((SelectExitEventArgs args) => SetColliders(true, colliderEnableDelay));
+    }
+
+    private IEnumerator SetColliders(bool value, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        foreach (var item in colliders)
+        {
+            item.enabled = value;
+        }
     }
 
     private void FixedUpdate()
