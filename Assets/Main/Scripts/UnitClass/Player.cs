b6a7c9b1219b0c898143f8df5e4306b3631d1ca2
SpherosGames <jbol1@st.noorderpoort.nl>
2024-10-08 11:21:27 +0200

    Working on health slider
    Better sword

--- Assets/Main/Scripts/UnitClass/Player.cs
+++ Assets/Main/Scripts/UnitClass/Player.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : Unit
 {
     [SerializeField] private string loseSceneName;
+    [SerializeField] private Slider healthSlider;
 
     public override void Die()
     {
