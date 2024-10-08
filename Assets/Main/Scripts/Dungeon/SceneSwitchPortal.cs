beece81ff7a7bc4fbd13dad8abe70722f6c92291
SpherosGames <jbol1@st.noorderpoort.nl>
2024-10-08 10:16:36 +0200

    Working on gameloop

---
+++ Assets/Main/Scripts/Dungeon/SceneSwitchPortal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitchPortal : MonoBehaviour
+{
+    [SerializeField] private int playerLayerNum;
+    [SerializeField] private string sceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == playerLayerNum)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
