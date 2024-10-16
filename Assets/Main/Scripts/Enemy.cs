c8169a6e4581216549bdc85b96ca473059222aa9
Joost <2006joost@gmail.com>
2024-10-16 13:26:13 +0200

    Fixed damage by sword
    Sword now does more damage when hit by golem.
    also added health bar for golem
    also added debug windows for console logs

--- Assets/Main/Scripts/Enemy.cs
+++ Assets/Main/Scripts/Enemy.cs
@@ -1,17 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Player : Unit
+public class Enemy : Unit
 {
-    [SerializeField] private string loseSceneName;
     [SerializeField] private Slider healthSlider;
 
     private void OnEnable()
     {
-       healthSlider.maxValue = MaxHealth;
+        healthSlider.maxValue = MaxHealth;
     }
 
     public override void Update()
@@ -21,8 +19,4 @@ public class Player : Unit
         healthSlider.value = Health;
     }
 
-    public override void Die()
-    {
-        SceneManager.LoadScene(loseSceneName);
-    }
 }
