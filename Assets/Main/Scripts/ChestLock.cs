0879cb2f675d46086b2c320ed23661aeb6f7190d
SpherosGames <jbol1@st.noorderpoort.nl>
2024-11-05 13:01:55 +0100

    Fixed Chest
    - Converted chest from custom grabbing system to xrinteractiontoolkit grabbing system
    - The chest can't open now when there is no key in the lock
    - Converted chestlock code from custom grabbing system to xrinteractiontoolkit

--- Assets/Main/Scripts/ChestLock.cs
+++ Assets/Main/Scripts/ChestLock.cs
@@ -1,29 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ChestLock : MonoBehaviour
 {
     [SerializeField] private Rigidbody ChestLid;
     [SerializeField] private Chest LockedChest;
     [SerializeField] private Rigidbody LockModel;
-    private bool BeenUnlocked = false;
-
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    CheckOpenLock(other);
-    //}
+    [SerializeField] private XRSocketInteractor socketInteractor;
+    [SerializeField] private InteractionLayerMask interactionLayerMask;
+    public bool BeenUnlocked = false;
 
     /// <summary>
     /// Checks if you can open the lock on trigger, opens it if you can
     /// </summary>
-    public void CheckOpenLock(GameObject Other)
+    public void CheckOpenLock(SelectEnterEventArgs args)
     {
         //If it isnt an item, it cant be a key
-        if (Other.GetComponent<Item>() == null) return;
+        if (args.interactableObject.transform.gameObject.GetComponent<Item>() == null) return;
 
         //If it is a key and the chest hasnt been openend
-        if (Other.GetComponent<Item>().ItemType == "Key" && !LockedChest.HasBeenOpened && !BeenUnlocked)
+        if (args.interactableObject.transform.gameObject.GetComponent<Item>().ItemType == "Key" && !LockedChest.HasBeenOpened && !BeenUnlocked)
         {
             //Sets the chests hinge limits higher
             JointLimits limits = ChestLid.GetComponent<HingeJoint>().limits;
@@ -31,9 +29,14 @@ public class ChestLock : MonoBehaviour
             ChestLid.GetComponent<HingeJoint>().limits = limits;
 
             //Makes the key unusable
-            Other.GetComponent<Item>().ItemType = "UsedKey";
+            args.interactableObject.transform.gameObject.GetComponent<Item>().ItemType = "UsedKey";
 
-            //BeenUnlocked = true;
+            args.interactableObject.transform.position = socketInteractor.attachTransform.position;
+            args.interactableObject.transform.rotation = socketInteractor.attachTransform.rotation;
+            args.interactableObject.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            socketInteractor.socketActive = false;
+
+            BeenUnlocked = true;
         }
     }
 }
