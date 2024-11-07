ed47f600bf16499019eddb2df5167ed89d3c10c6
Tempy <113506941+UsedTempy@users.noreply.github.com>
2024-11-07 14:11:40 +0100

    Bomb Script

--- Assets/Main/Scripts/Items/Bomb.cs
+++ Assets/Main/Scripts/Items/Bomb.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Bomb : MonoBehaviour {
     [SerializeField] private int TimeUntilExplosion;
+    private bool HandState = false; // False is right and true is left
 
-    void Throw() {
+    void ActivateBomb() {
+
+    }
+
+    void Select(SelectEnterEventArgs Args) {
+        GameObject controller = Args.interactor.transform.gameObject;
+        //controller.name;
+    }
+
+    void Start() {
 
     }
 }
