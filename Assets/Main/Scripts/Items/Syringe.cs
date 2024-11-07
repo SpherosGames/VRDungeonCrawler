892e8f2851ce44b35faa4628892f57b683330c4a
RudoVerschuren <rpverschuren@st.noorderpoort.nl>
2024-11-07 14:09:25 +0100

    Added comments to Syringe

--- Assets/Main/Scripts/Items/Syringe.cs
+++ Assets/Main/Scripts/Items/Syringe.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class Syringe : MonoBehaviour
 {
-    //To check if it touches the player
-    [SerializeField] LayerMask PlayerLayer;
-
+    //Makes it easier to have syringe varients, especially when working with chests
     public SyringeScriptableobject Scriptable;
 
     private Rigidbody RigidB;
@@ -20,15 +18,20 @@ public class Syringe : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        //Single use
         if (!BeenUsed)
         {
-            print(collision.gameObject.layer);
+            //If its the "Body" layer, I failed to use a layer variable for this.
             if(collision.gameObject.layer == 12)
             {
+                //If it is moving
                 if(RigidB.velocity.magnitude > 0.3f)
                 {
+                    //Freeze the syringe
                     transform.parent = collision.transform;
                     RigidB.isKinematic = true;
+
+
                     StartCoroutine(UseSyringe());
                 }
             }
@@ -37,11 +40,19 @@ public class Syringe : MonoBehaviour
 
     private IEnumerator UseSyringe()
     {
+        //Uses the item
+        BeenUsed = true;
+
         print("Used Syringe");
+
+        //Wait for a bit
         yield return new WaitForSeconds(Scriptable.TimeToUse);
+
+        //Drop syringe
         transform.parent = null;
         RigidB.isKinematic = false;
+
+        //Heal
         FindObjectOfType<Player>().TakeDamage(-Scriptable.InstantHealing);
-        BeenUsed = true;
     }
 }
--- Assets/Main/Scripts/Items/SyringeScriptableobject.cs
+++ Assets/Main/Scripts/Items/SyringeScriptableobject.cs
@@ -6,7 +6,12 @@ using UnityEngine;
 [System.Serializable]
 public class SyringeScriptableobject : ScriptableObject
 {
+    [Tooltip("Healing the player gets from using the item")]
     public float InstantHealing = 0;
+
+    [Tooltip("Permanent damage increase")]
     public float PermaDamageBoost = 0;
+
+    [Tooltip("The time until the effects take into use and the time until it drops the syringe out of you")]
     public float TimeToUse = 1;
 }
