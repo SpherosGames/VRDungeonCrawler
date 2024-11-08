b99e6dbb09b12987a391b874add8ff1923403a96
Tempy <113506941+UsedTempy@users.noreply.github.com>
2024-11-08 10:52:05 +0100

    Bomb Fixes

--- Assets/Main/Scripts/Items/Bomb.cs
+++ Assets/Main/Scripts/Items/Bomb.cs
@@ -6,18 +6,52 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Bomb : MonoBehaviour {
     [SerializeField] private int TimeUntilExplosion;
+    [SerializeField] private ParticleSystem ExplosionParticle;
+    [SerializeField] private ParticleSystem SparksParticle;
+    [SerializeField] private InputActionProperty leftActionProperty;
+    [SerializeField] private InputActionProperty rightActionProperty;
+
     private bool HandState = false; // False is right and true is left
+    private bool BombThrown = false;
+    private bool Held = false;
 
-    void ActivateBomb() {
-
+    private IEnumerator ActivateBomb() {
+        BombThrown = true;
+        SparksParticle.Play();
+        yield return new WaitForSeconds(TimeUntilExplosion);
+        ExplosionParticle.Play();
+        SparksParticle.Stop();
+        yield return new WaitForSeconds(0.4f);
+        Destroy(gameObject);
     }
 
-    void Select(SelectEnterEventArgs Args) {
+    public void Select(SelectEnterEventArgs Args) {
         GameObject controller = Args.interactor.transform.gameObject;
-        //controller.name;
+        if (controller.CompareTag("LeftController")) {
+            HandState = true;
+        }
+
+        if (controller.CompareTag("RightController")) {
+            HandState = false;
+        }
+
+        Held = true;
     }
 
-    void Start() {
+    public void DeSelect() {
+        Held = false;
+    }
 
+    void Update() {
+        if (Held != true) return;
+        if (leftActionProperty.action.ReadValue<float>() > 0.5f && HandState == true) {
+            if (BombThrown) return;
+            StartCoroutine(ActivateBomb());
+        }
+
+        if (rightActionProperty.action.ReadValue<float>() > 0.5f && HandState == false) {
+            if (BombThrown) return;
+            StartCoroutine(ActivateBomb());
+        }
     }
 }
